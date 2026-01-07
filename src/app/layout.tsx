eb1beb84e36ee64be87c82918834811f1002402e
Frendy Ard <frendyardiansyah@frendys-MacBook-Air.local>
2026-01-07 18:07:20 +0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Clodream | Dream in Clothing",
   description: "Web clothing store impianmu.",
+  icons: {
+    icon: "/clodream.png",
+    apple: "/clodream.png",
+  },
+  openGraph: {
+    title: "Clodream | Dream in Clothing",
+    description: "Web clothing store impianmu.",
+    images: ["/clodream.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["/clodream.png"],
+  },
 };
+
 
 export default function RootLayout({
   children,
